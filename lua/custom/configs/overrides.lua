5009fc612f17d0bc440a3c1e4fb1c64ee06428b2
michaelniu <michaelniu@tencent.com>
2023-07-12 16:49:25 +0800

    增加copilot

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -56,4 +56,17 @@ M.nvimtree = {
   },
 }
 
+-- Add the following in your custom/configs/overrides.lua file. You can configure copilot to meet your needs here.
+
+M.copilot = {
+  -- Possible configurable fields can be found on:
+  -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+  suggestion = {
+    enable = false,
+  },
+  panel = {
+    enable = false,
+  },
+}
+
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -60,6 +60,34 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
   -- }
+
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
+  },
 }
 
 return plugins
